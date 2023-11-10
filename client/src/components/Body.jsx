3dabfc42767ca1e796a9f5cf321aed8de9827898
Svastik Sharma <svastiksharma13@gmail.com>
2023-11-10 00:12:11 -0500

    added starter components

---
+++ client/src/components/Body.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Body() {
+
+    return (
+        <div>
+
+            <h2>
+                Trips
+            </h2>
+
+            <h3>Upcoming | OLD</h3>
+
+        </div>
+    )
+
+}
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Header() {
+
+    return (
+        <div>
+
+            <h1>
+                Travel Dashboard
+            </h1>
+
+        </div>
+    )
+}
