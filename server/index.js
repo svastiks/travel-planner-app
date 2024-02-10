bf1aa73eaec3dd1daa223202bde7cac64e575f86
Svastik Sharma <svastiksharma13@gmail.com>
2024-02-10 17:12:22 -0500

    conected to DB

--- server/index.js
+++ server/index.js
@@ -5,8 +5,19 @@ const express = require("express");
 
 const mongoose = require("mongoose");
 
+const env = require("dotenv");
+
 const app = express();
 
-app.listen(3000, () => {
-    console.log("Listening to port 3000")
+env.config();
+
+mongoose.connect(process.env.MONGO_CONNECTION).then(() => {
+    console.log("SUCCESS -> Conected to DB.")
 })
+
+app.listen(3000, () => {
+    console.log("SUCCESS -> Backend server started.")
+})
+
+
+
