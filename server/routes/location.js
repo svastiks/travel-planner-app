bb2e9f51afda91cae16b6cbe7c75c49ceeda76b9
Svastik Sharma <svastiksharma13@gmail.com>
2024-02-11 19:03:23 -0500

    more routes created

--- server/routes/location.js
+++ server/routes/location.js
@@ -1,6 +1,6 @@
 const router = require("express").Router(); // router
 
-const location = require("../models/Location"); // Location model
+const Location = require("../models/Location"); // Location model
 
 // Store selected location in DB
 
@@ -9,11 +9,31 @@ router.post("/", async (req, res) => {
 
     const newLocation = new Location(req.body); // this will create an object from the request body
 
-
+    try {
+        const saveLocation = await newLocation.save();
+        res.status(200).json(saveLocation);
+        console.log("SUCCESS -> Location added to DB.")
+    }
+    catch (err) {
+        console.log("FAILED -> Location added to DB.")
+        res.status(500).json(err);
+    }
 
 })
 
 // Retrieve PIN to display on client side
 
-// 
+router.get("/", async (req, res) => {
 
+    try {
+        const location = await Location.find();
+        res.status(200).json(location);
+        console.log("SUCCESS -> All location's retrieved.")
+    }
+    catch (err) {
+        console.log("FAILED -> Location added to DB.")
+        res.status(500).json(err);
+    }
+})
+
+module.exports = router; // export router
