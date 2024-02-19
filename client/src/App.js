5a182564d7154a9ac229333ff6fb24fc065b09a6
Svastik Sharma <svastiksharma13@gmail.com>
2024-02-18 21:00:57 -0500

    added axios

--- client/src/App.js
+++ client/src/App.js
@@ -4,26 +4,36 @@ import Body from './components/Body'
 import CountryCard from './components/CountryCard'
 import NextDestination from './components/NextDestination'
 import * as React from 'react'
-import Map from 'react-map-gl'
+import Map, { NavigationControl } from 'react-map-gl'
 import "mapbox-gl/dist/mapbox-gl.css"
+import axios from 'axios'
 
 // const env = require("dotenv");
 
 // env.config();
 
 function App() {
+
+  fetch
+
   return (
     <div>
 
       <Map
-        initialViewState={{}}
         //mapboxAccessToken={process.env.TRAVEL_APP_TOKEN}
-        style={{ width: "100vh", height: "100vh" }}
+        container={'map'}
+        projection={'globe'}
+        initialViewState={{}}
+        style={{ width: "100vw", height: "100vh" }}
         mapboxAccessToken='pk.eyJ1Ijoic3Zhc3Rpa3MiLCJhIjoiY2xzcXNzdGZrMGo4OTJpczN2bDlhYXFkZyJ9.UClNePM0ExbrQx0qBCQPMw'
         mapStyle="mapbox://styles/svastiks/clsqsyb7w04n501p22qi16i7o"
       >
 
+        <NavigationControl></NavigationControl>
+
       </Map>
+
+
 
       {/* <Header />
 
