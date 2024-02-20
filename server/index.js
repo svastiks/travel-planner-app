1a9a23afe51f890dd9b077a15d904892ab82951d
Svastik Sharma <svastiksharma13@gmail.com>
2024-02-20 00:47:29 -0500

    cors added

--- server/index.js
+++ server/index.js
@@ -9,6 +9,12 @@ const userRoute = require("./routes/users");
 
 const app = express();
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.use(express.json()); // send JSON data to the client
 env.config();
 
